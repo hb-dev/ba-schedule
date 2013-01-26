8db352a95fa54d9def12d5c70dad2f5596b33171
Henrik <dev@ubuntu.(none)>
2013-01-27 00:51:04 +0100

    asdd

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+$KCODE = 'u'
 # Initialize the rails application
 BaSchedule::Application.initialize!
+$KCODE = 'u'
