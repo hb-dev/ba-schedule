6d0eacb4009270264bd0ba2208ef11c2a2421a97
Henrik <dev@ubuntu.(none)>
2013-01-28 14:23:28 +0100

    asd

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -106,8 +106,9 @@ class SchedulesController < ApplicationController
       
       if @lessons 
         @calendar = Calendar.new
+        @calendar.product_id = "-//ba-stundenplan.herokuapp.com//iCal 1.0//DE"
         @calendar.custom_property("X-WR-CALNAME;VALUE=TEXT", "BA-Vorlesungsplan")
-        @calendar.custom_property("X-WR-TIMEZONE;VALUE=TEXT", "Europe/Berlin")        
+        @calendar.custom_property("X-WR-TIMEZONE;VALUE=TEXT", "Europe/London")        
         # @calendar.timezone do
           # timezone_id             "W. Europe Standard Time"
 #         
